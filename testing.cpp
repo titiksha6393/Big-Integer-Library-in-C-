6d961ef2507ec98e0db916788584d70f8888ace6
Titiksha Gupta <83998883+titiksha6393@users.noreply.github.com>
2022-09-08 22:27:43 +0530

    Create testing.cpp

---
+++ testing.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "bigInteger.h"
+using namespace std;
+
+int main(){
+	bigInteger a="3",b=-2;
+    cout<<"a = "<<a<<",b = "<<b<<endl;
+    cout<<"a+b = "<<a+b<<endl;
+    cout<<"a-b = "<<a-b<<endl;
+    cout<<"a*b = "<<a*b<<endl;
+    cout<<"a/b = "<<a/b<<endl;
+    cout<<"a%b = "<<a%b<<endl;
+    return 0;
+}
